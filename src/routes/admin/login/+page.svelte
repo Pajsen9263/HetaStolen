1fbef2cc73fbdecbb431822dcd91a4cbc7768aaf
CaptainFallaway <alexander.wollmer97@gmail.com>
2026-03-04 02:08:00 +0100

    Remove loading state and custom enhance callback

--- src/routes/admin/login/+page.svelte
+++ src/routes/admin/login/+page.svelte
@@ -15,8 +15,6 @@
 
 	// I don't really like that this isn't as typesafe as my implementation in Blocketia, I will refactor this.
 	let { form } = $props<{ form: { error: string } | null }>();
-
-	let loading = $state(false);
 </script>
 
 <div class="flex min-h-screen items-center justify-center">
@@ -26,17 +24,7 @@
 			<Card.Description>Enter the admin password to continue</Card.Description>
 		</Card.Header>
 		<Card.Content>
-			<form
-				method="POST"
-				use:enhance={() => {
-					loading = true;
-					return async ({ update }) => {
-						await update();
-						loading = false;
-					};
-				}}
-				class="space-y-4"
-			>
+			<form method="POST" use:enhance class="space-y-4">
 				<div class="space-y-2">
 					<Label for="adminSecret">Password</Label>
 					<Input
@@ -52,13 +40,7 @@
 					<p class="text-sm text-red-500">{form.error}</p>
 				{/if}
 
-				<Button type="submit" class="mt-4 w-full" disabled={loading}>
-					{#if loading}
-						Loading...
-					{:else}
-						Login
-					{/if}
-				</Button>
+				<Button type="submit" class="mt-4 w-full">Login</Button>
 			</form>
 		</Card.Content>
 	</Card.Root>
