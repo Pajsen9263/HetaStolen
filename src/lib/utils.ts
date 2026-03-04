b3b8a9c06fc95ab8c4a7f36d2ddd09b625ac02e0
CaptainFallaway <alexander.wollmer97@gmail.com>
2026-03-04 15:13:51 +0100

    Added maybenull type

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,5 +1,6 @@
 import { clsx, type ClassValue } from "clsx";
 import { twMerge } from "tailwind-merge";
+import * as v from "valibot";
 
 export function cn(...inputs: ClassValue[]) {
 	return twMerge(clsx(inputs));
@@ -11,3 +12,7 @@ export type WithoutChild<T> = T extends { child?: any } ? Omit<T, "child"> : T;
 export type WithoutChildren<T> = T extends { children?: any } ? Omit<T, "children"> : T;
 export type WithoutChildrenOrChild<T> = WithoutChildren<WithoutChild<T>>;
 export type WithElementRef<T, U extends HTMLElement = HTMLElement> = T & { ref?: U | null };
+
+export type MaybeNull<T> = {
+	[K in keyof T]: T[K] | undefined;
+};
