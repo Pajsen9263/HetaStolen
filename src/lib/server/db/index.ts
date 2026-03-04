78e587fdad14288dd99cbc782156527d98ecb3d9
CaptainFallaway <alexander.wollmer97@gmail.com>
2026-03-04 22:11:35 +0100

    Enable WAL mode for sqlite for faster concurrent / faster reads

--- src/lib/server/db/index.ts
+++ src/lib/server/db/index.ts
@@ -3,6 +3,12 @@ import { drizzle } from "drizzle-orm/bun-sqlite";
 
 const db = drizzle(env.DB_FILE_NAME);
 
+try {
+	db.$client.run("PRAGMA journal_mode = WAL;");
+} catch (e) {
+	console.error("Failed to enable WAL", e);
+}
+
 export default db;
 
 export * from "./schema.ts";
