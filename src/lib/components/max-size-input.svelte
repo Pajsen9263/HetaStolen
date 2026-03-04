ae8f01e2a2547108cc7eacf45dd5eb5542ac110e
CaptainFallaway <alexander.wollmer97@gmail.com>
2026-03-04 22:09:10 +0100

    Made a reusable component for max size text inputs

---
+++ src/lib/components/max-size-input.svelte
@@ -0,0 +1,37 @@
+<script lang="ts">
+	import * as InputGroup from "@/ui/input-group";
+	import type { ComponentProps } from "svelte";
+
+	let {
+		max,
+		value = $bindable(""),
+		maxSizeExceeded = $bindable(false),
+		class: className,
+		...props
+	}: {
+		max: number;
+		value?: string;
+		maxSizeExceeded?: boolean;
+	} & Omit<ComponentProps<typeof InputGroup.Input>, "type" | "files"> = $props();
+
+	let valueLength = $derived(value.length);
+
+	$effect(() => {
+		maxSizeExceeded = valueLength > max;
+	});
+</script>
+
+<InputGroup.Root>
+	<InputGroup.Input
+		type="text"
+		{...props}
+		class={[maxSizeExceeded && "text-destructive", className]}
+		bind:value
+		required
+	/>
+	<InputGroup.Addon align="inline-end">
+		<span class={["font-semibold", "text-sm", maxSizeExceeded ? "text-destructive" : "text-muted"]}>
+			{valueLength} / {max}
+		</span>
+	</InputGroup.Addon>
+</InputGroup.Root>
